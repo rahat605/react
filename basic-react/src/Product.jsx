aa8a6a37c7b815aae57d73d89675003cfba7d6fe
rahat605 <mdrahatrion2@gmail.com>
2025-10-29 04:26:53 +0600

    offerCard

--- basic-react/src/Product.jsx
+++ basic-react/src/Product.jsx
@@ -2,8 +2,8 @@ import Price from "./price";
 import "./Product.css";
 
 function Product({title, idx }) {
-  let oldPrices = ["12,999","15,000","29,000","4,900"];
-  let newPrices = ["14,000","13,500","32,990","7,000"];
+  let oldPrices = ["18,999","15,999","37,999","7,999"];
+  let newPrices = ["14,999","12,999","30,999","6,999"];
   let description = [["Motherboard","B450M Series"],["Processor","6 Core 6 Thread"],["Graphics Card","OC Edition"],["Storage Device","NvME"]]
 
     return (
