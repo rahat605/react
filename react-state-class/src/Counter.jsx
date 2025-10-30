191de190180e2d18f6378c6183791e318d0b550d
rahat605 <mdrahatrion2@gmail.com>
2025-10-30 08:37:16 +0600

    state

---
+++ react-state-class/src/Counter.jsx
@@ -0,0 +1,15 @@
+export default function Counter() {
+  let count = 0;
+
+  function incCount() {
+    count = count + 1;
+    console.log(count);
+  }
+
+  return (
+    <div>
+      <h2>Count = {count}</h2>
+      <button onClick={incCount}>Increase Count</button>
+    </div>
+  );
+}
