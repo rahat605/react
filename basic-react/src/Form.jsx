23d0ca7d2a3ee9942629e97ed4ce9203f9464af9
rahat605 <mdrahatrion2@gmail.com>
2025-11-14 03:00:22 +0600

    form

--- basic-react/src/Form.jsx
+++ basic-react/src/Form.jsx
@@ -1,35 +1,57 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("");
-  let [lastName, setLastName] = useState("");
+  let [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    password:"",
+  });
 
-  let handleChange = (event) => {
-    setFullName(event.target.value);
+  let handleInputChange = (event) => {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]:[event.target.value] };
+    });
   };
 
-   let handleLastNameC = (event) => {
-    setLastName(event.target.value);
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    setFormData({
+      firstName: "",
+      lastName: "",
+      password:"",
+    });
   };
 
   return (
-    <form action="" className="formForUser">
+    <form action="" onSubmit={handleSubmit}>
       <label htmlFor="firstname">First name : </label>
       <input
         type="text"
         placeholder="Type your first name"
-        value={fullName}
-        onChange={handleChange}
+        value={formData.firstName}
+        onChange={handleInputChange}
         id="firstname"
+        name="firstName"
       />
       <br />
       <label htmlFor="lastname">Last name : </label>
       <input
         type="text"
         placeholder="Type your last name"
-        value={lastName}
-        onChange={handleLastNameC}
+        value={formData.lastName}
+        onChange={handleInputChange}
         id="lastname"
+        name="lastName"
+      />
+      <br />
+      <label htmlFor="password">Password : </label>
+      <input
+        type="password"
+        placeholder="Type your password"
+        value={formData.password}
+        onChange={handleInputChange}
+        id="password"
+        name="password"
       />
       <button>Search</button>
     </form>
